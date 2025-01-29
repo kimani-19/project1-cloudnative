08485e3f299cb04e3f467c987f2d3075e3af1361
kimani-19 <147687023+kimani-19@users.noreply.github.com>
2025-01-29 18:36:35 -0500

    Files Uploaded

---
+++ main.py
@@ -0,0 +1,83 @@
+import time
+import os
+from google.cloud import storage
+from flask import Flask, request, redirect, send_file, Response
+import io
+os.makedirs('files', exist_ok = True)
+
+app = Flask(__name__)
+
+storage_client=storage.Client()
+Bucket_name="imgbucket-kimanimunn"
+
+@app.get("/hello")
+def hello():
+    """Return a friendly HTTP greeting."""
+    who = request.args.get("who", default="World")
+    time.sleep(5)
+    return f"Hello {who}!\n"
+
+@app.route('/')
+def index():
+    index_html = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Upload Image</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body class="container py-5">
+        <h1 class="text-center text-primary">Image Upload Portal</h1>
+        <h2 class="text-center">Upload an Image</h2>
+        <form method="post" enctype="multipart/form-data" action="/upload" class="mb-4 text-center">
+            <input type="file" name="form_file" accept="image/jpeg" class="form-control mb-2" required>
+            <button class="btn btn-primary">Upload</button>
+        </form>
+        <h3 class="mt-4">Uploaded Images</h3>
+        <ul class="list-group">
+    """
+    
+    for file in list_files():
+        index_html += f'<li class="list-group-item"><a href="/files/{file.name}">{file.name}</a></li>'
+    
+    index_html += """
+        </ul>
+    </body>
+    </html>
+    """
+    return index_html
+
+@app.route('/upload', methods=["POST"])
+def upload():
+
+    file = request.files['form_file']  # item name must match name in HTML form
+    #file.save(os.path.join("./files", file.filename))
+    bucket = storage_client.bucket(Bucket_name)
+    blob_image = bucket.blob(file.filename)
+    blob_image.upload_from_file(file_obj=file, rewind=True)
+    return redirect("/")
+
+@app.route('/files')
+def list_files():
+    files = storage_client.list_blobs(Bucket_name)
+    jpegs = []
+    for file in files:
+        if file.name.lower().endswith(".jpeg") or file.name.lower().endswith(".jpg"):
+            jpegs.append(file)
+    
+    return jpegs
+
+@app.route('/files/<filename>')
+def get_file(filename):
+  bucket = storage_client.bucket(Bucket_name)
+  blob = bucket.blob(filename)
+  file_data = blob.download_as_bytes()
+  return Response(io.BytesIO(file_data), mimetype='image/jpeg')
+
+if __name__ == "__main__":
+    # Development only: run "python main.py" and open http://localhost:8080
+    # When deploying to Cloud Run, a production-grade WSGI HTTP server,
+    # such as Gunicorn, will serve the app.
+    app.run(host="localhost", port=8080, debug=True)
