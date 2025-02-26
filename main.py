0f713466fd9ae90f33aa3749591d153c5fe660ae
kimani-19 <147687023+kimani-19@users.noreply.github.com>
2025-02-26 15:18:47 -0500

    Edited and updated files

--- main.py
+++ main.py
@@ -3,12 +3,41 @@ import os
 from google.cloud import storage
 from flask import Flask, request, redirect, send_file, Response
 import io
+from PIL import Image
 os.makedirs('files', exist_ok = True)
+import json
+import google.generativeai as genai
 
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+
+
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-flash",
+#   generation_config=generation_config,
+  # safety_settings = Adjust safety settings
+  # See https://ai.google.dev/gemini-api/docs/safety-settings
+)
+
+PROMPT = " Give me a a one line title and description for this image, in a json format. "
+
+def upload_to_gemini(path, mime_type=None):
+  """Uploads the given file to Gemini.
+
+  See https://ai.google.dev/gemini-api/docs/prompting_with_media
+  """
+  file = genai.upload_file(path, mime_type=mime_type)
+  print(f"Uploaded file '{file.display_name}' as: {file.uri}")
+  # print(file)
+  return file
+
+
+
+# print(response)
 app = Flask(__name__)
 
 storage_client=storage.Client()
-Bucket_name="imgbucket-kimanimunn"
+Bucket_name = os.environ.get("BUCKET_NAME")
 
 @app.get("/hello")
 def hello():
@@ -57,6 +86,22 @@ def upload():
     bucket = storage_client.bucket(Bucket_name)
     blob_image = bucket.blob(file.filename)
     blob_image.upload_from_file(file_obj=file, rewind=True)
+    file.save(os.path.join("",file.filename))
+    response = model.generate_content([Image.open(file), PROMPT])
+    print(response.text)
+    index_1=response.text.index("{")
+    index_2=response.text.index("}")
+    response_string=response.text[index_1:index_2+1]
+    print (response_string,type(response_string))
+    json_response=json.loads(response_string)
+    print(json_response)
+    file_name = file.filename.split(".")[0]+".json"
+    # Write JSON data to a file
+    with open(file_name, "w") as json_file:
+        json.dump(json_response, json_file, indent=4)
+    blob_text = bucket.blob(file.filename.split(".")[0]+".json")
+    blob_text.upload_from_filename(file.filename.split(".")[0]+".json")
+
     return redirect("/")
 
 @app.route('/files')
@@ -71,10 +116,25 @@ def list_files():
 
 @app.route('/files/<filename>')
 def get_file(filename):
-  bucket = storage_client.bucket(Bucket_name)
-  blob = bucket.blob(filename)
-  file_data = blob.download_as_bytes()
-  return Response(io.BytesIO(file_data), mimetype='image/jpeg')
+    bucket = storage_client.bucket(Bucket_name)
+    blob = bucket.blob(filename.split(".")[0]+".json")
+    file_data = blob.download_as_bytes()
+    file_data = json.loads(file_data.decode('utf-8'))
+    print(file_data)
+    html= f"""
+        <img src='/images/{filename}'>
+        <h1>Tiltle:{file_data['title']}</h1>
+        <p>Description:{file_data['description']}</p>
+    """
+    return html
+#   return Response(io.BytesIO(file_data), mimetype='image/jpeg')
+
+@app.route('/images/<imagename>')
+def view_image(imagename):
+    bucket = storage_client.bucket(Bucket_name)
+    blob = bucket.blob(imagename)
+    file_data = blob.download_as_bytes()
+    return Response(io.BytesIO(file_data), mimetype='image/jpeg')
 
 if __name__ == "__main__":
     # Development only: run "python main.py" and open http://localhost:8080
