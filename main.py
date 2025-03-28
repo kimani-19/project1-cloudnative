5cf375a94be52de02580067885937172ee693ed0
kimani-19 <147687023+kimani-19@users.noreply.github.com>
2025-03-28 12:03:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def index():
         <title>Upload Image</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
-    <body bgcolor="blue" class="container py-5">
+    <body  class="container py-5" bgcolor="blue">
         <h1 class="text-center text-primary">Image Upload Portal</h1>
         <h2 class="text-center">Upload an Image</h2>
         <form method="post" enctype="multipart/form-data" action="/upload" class="mb-4 text-center">
